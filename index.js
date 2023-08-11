6fcccbe315dfe71e720946a875c550ad95d45d08
mhtajan <marktajan@icloud.com>
2023-08-11 20:46:45 +0800

    fixed dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios')
 const app = express();
-require('dotenv').config
+require('dotenv').config();
 const port = 3000;
 
 // Middleware parser
